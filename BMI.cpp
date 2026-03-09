2001b9cba258515de2caab97b0c562e2fe6a2eaa
alfarisf1704-hub <alfarisf1704@gmail.com>
2026-03-09 19:23:54 +0700

    menambahkan deklarasi status badan

--- BMI.cpp
+++ BMI.cpp
@@ -5,3 +5,15 @@ float BMI(float b, float t)
 {
     return b / (t * t);
 }
+
+string Status_badan(float bm)
+{
+    if (bm < 18.5)
+        return "Berat Badan Kurang";
+    if (bm < 25)
+        return "Berat Badan Normal";
+    if (bm < 30)
+        return "Berat Badan Kelebihan";
+    else
+        return "Obesitas";
+}
